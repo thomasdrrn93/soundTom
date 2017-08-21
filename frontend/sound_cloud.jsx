a3ac3fe103f9a6bf30cd87d168a24901160186c1
Thomas Dorrian <thomasdrrn93@gmail.com>
2017-08-20 20:02:03 -0400

    add routes

---
+++ frontend/sound_cloud.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDom.render(<h1>Test</h1>, document.getElementById('root'));
+});
